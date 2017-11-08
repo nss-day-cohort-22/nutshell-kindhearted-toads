1e44e7a949bcef6cddc057143f861a3e67fd96d4
Penny Caudle <pennycaudle@Pennys-MacBook-Pro.local>
2017-11-08 13:14:35 -0600

    login and new user creation handling

---
+++ scripts/createNewUser.js
@@ -0,0 +1,25 @@
+// Author: Sean Williams
+//Purpose: Create a new user and give it to the database handler then set session storage with user ifo
+
+
+const { validateUser, validateNewUser } = require("./loginValidation")
+const NutshellDatabase = require("./database")
+const setData = require("./dataSetter")
+
+
+const createNewUser = function (userName, email) {
+    if (validateNewUser(userName, email, NutshellDatabase.users)) {
+        const newUser = userTableFactory({ "userName": userName, "email": email })
+        const newUserArray = NutshellDatabase.users.push(newUser)
+        setData(newUserArray, "users")
+
+        const storedUserInfo = JSON.stringify({ "userID": newUser.id, "userName": newUser.userName, "isEditing": false })
+        sessionStorage.setItem("userInfo", storedUserInfo)
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+module.exports = createNewUser
---
+++ scripts/login.js
@@ -0,0 +1,21 @@
+// Author: Sean Williams
+//Purpose: Find user and set session storage with needed info
+
+
+const { validateUser, validateNewUser } = require("./loginValidation")
+const NutshellDatabase = require("./database")
+
+
+const login = function (userName, email) {
+    const user = validateUser(userName, email, NutshellDatabase.users)
+    if (user) {
+        const storedUserInfo = JSON.stringify({ "userID": user.id, "userName": user.userName, "isEditing": false })
+        sessionStorage.setItem("userInfo", storedUserInfo)
+        return true
+    }
+    else {
+        return false
+    }
+}
+
+module.exports = login
