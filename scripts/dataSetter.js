d1ffddb63f14d0097f4fa76e09b5972ae8ee3445
Greg Lawrence <gregaudio@gmail.com>
2017-11-08 13:10:28 -0600

    created two modules, one dataSetter and one dataGetter which will also create a db if one doesn't exit

--- scripts/dataSetter.js
+++ scripts/dataSetter.js
@@ -1,23 +1,23 @@
 // Greg - DataSetter will take in data as input and set it to local storage.
 
+// get a copy of the most recent version of DB in Local Storage from the database module
+const DB = require("./database")
+
 
 // parameters will be the array to put into the database and a string that states the label/contents of the array
-
-function setData (arrayToSet, labelOfArray) {
-    // get a copy of the most recent version of DB in Local Storage
-    let DB = JSON.parse(localStorage.getItem("DB"))
+function setData (arrayToSet, stringLabelOfArray) {
 
     // iterate over the DB and look for the property that matches the data to set.
     for (let key in DB) {
         // if property matches, overwrite the contents of the DB with the updated array
-        if (key === labelOfArray) {
+        if (key === stringLabelOfArray) {
             DB.key = arrayToSet
         }
     }
 
     // set the updated DB into local storage
-    localStorage.setItem("DB", JSON.stringify(DB))
+    localStorage.setItem("NutshellDatabase", JSON.stringify(DB))
 
 }
 
-modedule.exports = setData
+module.exports = setData
--- scripts/database.js
+++ scripts/database.js
@@ -1,28 +1,46 @@
-const NutshellDatabase = {
-    "users": [
-        { "id": 1, "username": "Steve", "email": "me@me.com", "password": 123 },
-        { "id": 2, "username": "Mark", "email": "you@you.com", "password": 123 },
-        { "id": 3, "username": "Krista", "email": "foo@foo.com", "password": 123 }
-    ],
-    "messages": [
-        { "id": 1, "userId": 1, "content": "What's up?", "timestamp": 0}
-    ],
-    "news": [
-        { "id": 1, "userId": 1, "url": "https://www.quantamagazine.org/newfound-wormhole-allows-information-to-escape-black-holes-20171023/", "title": "Check out this recent discovery about workholes", "synopsis": "wormholes", "timestamp": 0 },
-        { "id": 2, "userId": 2, "url": "www.google.com", "title": "Check out this great search site", "synopsis": "search", "timestamp": 0 },
-        { "id": 3, "userId": 3, "url": "www.github", "title": "A great repo site", "synopsis": "repos", "timestamp": 0 }
-    ],
-    "friends": [
-        { "id": 1, "userId": 1, "friendId": 3 },
-        { "id": 2, "userId": 1, "friendId": 2 }
-    ],
-    "tasks": [
-        { "id": 1, "userId": 3, "task": "Take out garbage", "complete": false, "completionDate": 0, "timestamp": 0 },
-        { "id": 2, "userId": 1, "task": "Take out garbage, Steve", "complete": false, "completionDate": 0, "timestamp": 0 }
-    ],
-    "events": [
-        {"id": 1, "userId": 1, "name": "Foo Fighters", "eventDate": 0, "location": "Bridgestone", "timestamp": 0 }
-    ]
+
+function getDatabase () {
+
+    let DB = JSON.parse(localStorage.getItem("NutshellDatabase")) || null
+
+    if (DB) {
+        console.log("Database Found")
+        return DB
+    } else {
+        console.log("No Database Found")
+        DB = {
+            "users": [
+                // eventually remove these placeholder accounts
+                { "id": 1, "username": "Steve", "email": "me@me.com", "password": 123 },
+                { "id": 2, "username": "Mark", "email": "you@you.com", "password": 123 },
+                { "id": 3, "username": "Krista", "email": "foo@foo.com", "password": 123 }
+            ],
+            "messages": [
+                { "id": 1, "userId": 1, "content": "This is where a chat message will display", "timestamp": 0}
+            ],
+            "news": [
+                { "id": 1, "userId": 1, "url": "https://www.quantamagazine.org/newfound-wormhole-allows-information-to-escape-black-holes-20171023/", "title": "This is an example of a News Article", "synopsis": "put a summary of the article here", "timestamp": 0 },
+                // { "id": 2, "userId": 2, "url": "www.google.com", "title": "Check out this great search site", "synopsis": "search", "timestamp": 0 },
+                // { "id": 3, "userId": 3, "url": "www.github", "title": "A great repo site", "synopsis": "repos", "timestamp": 0 }
+            ],
+            "friends": [
+                // eventually remove these placeholder friend connections
+                { "id": 1, "userId": 1, "friendId": 3 },
+                { "id": 2, "userId": 1, "friendId": 2 }
+            ],
+            "tasks": [
+                { "id": 1, "userId": 3, "task": "Task example :: Take out garbage", "complete": false, "completionDate": 0, "timestamp": 0 },
+                // { "id": 2, "userId": 1, "task": "Take out garbage, Steve", "complete": false, "completionDate": 0, "timestamp": 0 }
+            ],
+            "events": [
+                {"id": 1, "userId": 1, "name": "Event example :: Foo Fighters", "eventDate": 0, "location": "Bridgestone", "timestamp": 0 }
+            ]
+        }
+
+        localStorage.setItem("NutshellDatabase", JSON.stringify(DB))
+        return DB
+    }
 }
 
-modules.export = NutshellDatabase;
+
+module.exports = getDatabase;
