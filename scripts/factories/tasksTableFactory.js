cb2c1cf6605bd128225b48913709e2aac58c4af7
Christopher Miller <camiller.yr@gmail.com>
2017-11-08 13:56:16 -0600

    fixed stupid shit

--- scripts/factories/tasksTableFactory.js
+++ scripts/factories/tasksTableFactory.js
@@ -7,7 +7,7 @@ tasksIdGenerator = idGenerator()
 
 const taskTableFactory = taskInfoObject => {
     Object.create(null, {
-        "id" : {value: tasksIdGenerator.next.value(), enumerable: true, writable: true},
+        "id" : {value: tasksIdGenerator.next().value, enumerable: true, writable: true},
         "timeStamp" : {value: Date.now(), enumerable: true, writable: true},
         "userID" : {value: taskInfoObject.userID, enumerable: true, writable: true},
         "taskName" : {value: taskInfoObject.taskName, enumerable: true, writable: true},
