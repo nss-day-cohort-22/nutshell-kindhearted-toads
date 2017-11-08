db4dbf11e262cecae4ac272415f73884dee32ce4
krysmathis <krysmathis@gmail.com>
2017-11-08 15:36:47 -0600

    updated logininit

---
+++ scripts/taskGenerateWidget.js
@@ -0,0 +1,19 @@
+/**
+ * Krys Mathis
+ * Generate the mark-up for the task widget
+ */
+const taskData = [];
+const userId = 1;
+
+const taskGenerateTaskWidget = function(taskData, userId) {
+    
+    let html = `
+        <header class="taskWidget__header">Tasks</header>
+            <div class="taskContainer">
+            </div>
+        <div class=taskWidget__input">
+            <input class="taskWidget__txt" type="text">
+            <button class="taskWidget__btnSubmit">Submit</button>
+        </div>
+    `
+};
