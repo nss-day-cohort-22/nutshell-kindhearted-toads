b5bd9b57dc2d44e6999c5843465ad6493bac1f1f
krysmathis <krysmathis@gmail.com>
2017-11-09 16:00:41 -0600

    boxes change

--- scripts/taskWidget/addListeners.js
+++ scripts/taskWidget/addListeners.js
@@ -14,24 +14,39 @@ document.addEventListener("keyup", (e)=> {
             const taskContainer = document.querySelector(".taskContainer");
             //taskContainer.removeChild(e.target);
 
-            taskContainer.appendChild(
-                div({className: "task__desc", "id": "5"},txt)
-            );
+            taskContainer.appendChild(div({"className": "task"},
+                input({"type": "checkbox", "className": "task__checkbox"}),
+                span({"className": "task__desc"}, `${txt}`)));
             // create new element
         }
+    }
+
+    if (e.target.className === "task__input") {
+        let txt = e.target.value;
+        let parent = e.target.parentNode;
+        let replaceSpan = span({"className": "task__desc"},txt);
+        parent.replaceChild(replaceSpan,e.target);
     }
 });
 
 document.addEventListener("click", function(e){
+    
     if (e.target.className === "task__desc") {
-        let child = e.target;
-        let i = 0;
-        debugger;
-        while((child === child.previousSibling) !== null) {
-            i++
-        }
-        
-        console.log(i);
+        let txt = e.target.innerHTML;
+        console.log(e.target);
+        let parent = e.target.parentNode;
+        console.log(txt);
+        const inputDiv = document.createElement("div");
+        inputDiv.className = "task";
+        const checkBox = input({"type":"checkbox", "className": "task__checkbox"});
+        const inputBox = document.createElement("input");
+        inputBox.value = txt;
+        inputBox.className = "task__input";
+        inputDiv.appendChild(checkBox);
+        inputDiv.appendChild(inputBox);
+
+        parent.replaceChild(inputBox,e.target);
+
     }
 });
 
