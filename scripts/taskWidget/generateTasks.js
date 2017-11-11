0096ab847f213e3d169ec3bee791156ba0a75416
krysmathis <krysmathis@gmail.com>
2017-11-11 05:10:45 -0600

    updated styling and added completion date to task widget

--- scripts/taskWidget/generateTasks.js
+++ scripts/taskWidget/generateTasks.js
@@ -13,6 +13,7 @@ const generateTasks = function(tasks) {
         container.appendChild(newDiv);
         newDiv.innerHTML = `<input type="checkbox" class="task__checkbox">
         <div class="task__desc" data-id="${task.id}" data-user-id="${task.userId}">${task.taskName}</div>
+        <div class="task__completion-date" data-id="${task.id}" data-user-id="${task.userId}">${task.completionDate}</div>
         `
     })
 }
