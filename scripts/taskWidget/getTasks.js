3777c3beb567c9921f7cdc6d465d3d16a50707a2
krysmathis <krysmathis@gmail.com>
2017-11-11 05:26:37 -0600

    getTasks returns sorted tasks by id

--- scripts/taskWidget/getTasks.js
+++ scripts/taskWidget/getTasks.js
@@ -6,7 +6,9 @@ const database = require("../database");
 
 const getTasks = function(user) {
     // get incomplete tasks for user
-    let tasks = database().tasks.filter(t=> t.userId === user.userId && !t.complete)
+    let tasks = database().tasks
+        .filter(t=> t.userId === user.userId && !t.complete)
+        .sort((f,s)=> f.id - s.id);
     return tasks;
 }
 
