223bfa650355ee5466a11cf89806b0cdeb318564
Greg Lawrence <gregaudio@gmail.com>
2017-11-14 12:36:49 -0600

    refactored editBtn to show on click. It hides when the user has entered their new msg or when they user clicks again on the msg

--- scripts/chatWidget/chatEventListener.js
+++ scripts/chatWidget/chatEventListener.js
@@ -63,7 +63,7 @@ const createChatListener = (chatWidget) => {
         }
     }) 
 
-
+    // event listener to check if user clicked the "Enter" key in the input field
     chatInputField.addEventListener("keyup", event => {
 
         // check if enter key is pressed inside input field and that the input field isn't empty, if so, run createChatMsg function
@@ -72,10 +72,10 @@ const createChatListener = (chatWidget) => {
         }
     })
     
-    // event listener to check if user has clicked edit btn on a message
+    // event listener to check if user has clicked edit button OR a username on a message
     chatContainerEl.addEventListener("click", event => {
         
-        // check if event.target is a btn
+        // check if event.target is the Edit Button
         if (event.target.id.startsWith("editBtn_")) {
             let composeChatInput = document.querySelector(".chatWidget__text")
             
@@ -99,12 +99,13 @@ const createChatListener = (chatWidget) => {
                     addChatBtnEl.textContent = "Save"
                 }
             }
-            //console.log("msgToEditFromDB = ", msgToEditFromDB)
 
             // set editMode to true
             editMode = true
-            //Set the current article variable to the newly edited message object. This will later be passed into a function to write it to the database. 
+
+            // Set the current article variable to the newly edited message object. This will later be passed into a function to write it to the database. 
             currentMessage = msgToEditFromDB
+
         }
 
         // event listener to listen for click on userName
@@ -117,10 +118,11 @@ const createChatListener = (chatWidget) => {
             if (chatWidget.user.userId !== userIdClicked)
                 addFriendPrompt(chatWidget, userIdClicked, userNameClicked)
         }
+
     })
     
     // mouseover event to display an edit button 
-    chatContainerEl.addEventListener("mouseover", event => {
+    chatContainerEl.addEventListener("click", event => {
         // check if the target has a next sibling
         if (event.target.nextElementSibling) {
             // check if the next sibling element is the Edit Button
@@ -131,19 +133,5 @@ const createChatListener = (chatWidget) => {
         }
     })
 
-    // mouseout event to hide the edit button after mouseover is done
-    chatContainerEl.addEventListener("mouseout", event => {
-        console.log(event)
-        // if (event.target.className === "chatWidget__msg" && event.target.dataset.msgId === event.target.nextElementSibling.dataset.msgId) {
-        //     // check if the target has a next sibling
-        if (event.target.nextElementSibling) {
-            // check if the next sibling element is the Edit Button
-            let nextSiblingId = event.target.nextElementSibling.id
-            if (nextSiblingId.startsWith("editBtn_")) {
-                event.target.nextElementSibling.classList.toggle("hidden")
-            }
-        }
-        // }
-    })
 }
 module.exports = createChatListener
