7294f0c785c15310e08f21595cd4138e0ed9784f
krysmathis <krysmathis@gmail.com>
2017-11-15 13:50:23 -0600

    updated styling on login buttons

--- scripts/toaster/toaster.js
+++ scripts/toaster/toaster.js
@@ -63,19 +63,6 @@ const Toaster = function() {
             op += op * 0.1;
         }, 5);
     }
-
-    function fade(element) {
-        var op = 1;  // initial opacity
-        var timer = setInterval(function () {
-            if (op <= 0.1){
-                clearInterval(timer);
-                element.style.display = "none";
-            }
-            element.style.opacity = op;
-            element.style.filter = "alpha(opacity=" + op * 100 + ")";
-            op -= op * 0.1;
-        }, 50);
-    }
     
     return Object.create(null, {
         "makeToast": {
